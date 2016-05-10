b0132da0798407a5d113b0d50e56198f24ae026b
Andrii Melnykov <andy.melnikov@gmail.com>
2016-05-10 08:58:38 +0300

    Normalized benchmark time

--- benchmark/app.ts
+++ benchmark/app.ts
@@ -12,24 +12,31 @@ client.registerWorker(tube, (job) => {
 	setTimeout(() => job.end(job.payload.toUpperCase()), 100)
 })
 
-function pushJobs() {
+function pushJobs(sz) {
 		var jobs = []
-		for (var i = 0; i < 50; i++) jobs.push(client.submitJob(tube, 'abc'))
+		for (var i = 0; i < sz; i++) jobs.push(client.submitJob(tube, 'abc'))
 		return Promise.all(jobs)
 }
 
-var bench = new Benchmark('test abraxas', {
+function goBench(sz)
+{
+
+
+var bench = new Benchmark('abraxas-' + sz, {
 	'defer': true,
 	'maxTime' : 60,
 	'minRuns' : 20,
 	'initCount' : 3,
-	'fn': (deferred) => pushJobs().then(() => deferred.resolve())
+	'fn': (deferred) => pushJobs(sz).then(() => deferred.resolve())
 })
 .on('cycle', function(event) { process.stdout.write('\r' + String(event.target)) + "            " })
 .on('complete', function(event) {
-	var summary = sumStats(bench.stats.sample)
+	var summary = sumStats(bench.stats.sample.map((x) => x / sz))
 	console.log('\n')
-	console.log("%s,%s,%s", summary.median.toFixed(3), (summary.median - summary.q1).toPrecision(2), (summary.q3 - summary.median).toPrecision(2))
-})
+	console.log("%s,%s,%s", summary.median.toPrecision(3), (summary.median - summary.q1).toPrecision(2), (summary.q3 - summary.median).toPrecision(2))
+	})
+	return bench
+}
 
-pushJobs().then(() => pushJobs().then(() => bench.run({async: false})))
+pushJobs(150).then(() => pushJobs(150).then(() => goBench(100).run({async: false})))
+
