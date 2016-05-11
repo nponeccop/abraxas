97b8d9ed2c147a18cc1005bdc23e63ddd50cccb7
streamcode9@gmail.com <streamcode9@gmail.com>
2016-05-12 00:12:52 +0300

    Benchmarks are saved to Jypiter python file

--- benchmark/app.ts
+++ benchmark/app.ts
@@ -8,35 +8,73 @@ var tube: string = 'test_tube'
 
 ab.Server.listen()
 
-var client = ab.Client.connect({ maxJobs: 10, defaultEncoding: 'utf8' })
-client.registerWorker(tube, (job) => {
-	setTimeout(() => job.end(job.payload.toUpperCase()), 100)
-})
+function createPythonFile(suiteResults, benchmarksOpts) {
+	var data = '';
+	for (var i = 0; i < suiteResults.length; i++) {
+		var bench = suiteResults[i];
+		var benchmarkOptions = benchmarksOpts[i];
+		var summary = sumStats(bench.stats.sample.map((x) => x / benchmarkOptions.jobsCount))
+		var name = bench.name
+		var v1 = summary.median.toPrecision(3)
+		var v2 = (summary.median - summary.q1).toPrecision(2)
+		var v3 = (summary.q3 - summary.median).toPrecision(2)
+		data += `"${name}":[${v1}/1,${v2}/2,${v3}/3],`
+	}
+	
+	var code = `%matplotlib notebook\
+				\n\
+				\nimport pandas as pd\
+				\nimport numpy as np\
+				\nimport matplotlib\
+				\n\
+				\nfrom matplotlib import pyplot as plt\
+				\nimport seaborn as sns\
+				\ndf = pd.DataFrame({${data}})\
+				\ndf.plot(kind="bar")`
+	fs.writeFile('results.py', code, 'utf8', err => { if (err) throw err; console.log('It\'s saved!') })
+}
 
-function pushJobs(sz) {
+function pushJobs(jobsCount, abraxasOptions) {
+	var client = ab.Client.connect(abraxasOptions)
+	client.registerWorker(tube, (job) => {
+		setTimeout(() => job.end(job.payload.toUpperCase()), 100)
+	})
+
 	var jobs = []
-	for (var i = 0; i < sz; i++) jobs.push(client.submitJob(tube, 'abc'))
+	for (var i = 0; i < jobsCount; i++) jobs.push(client.submitJob(tube, 'abc'))
 	return Promise.all(jobs)
 }
 
-function goBench(sz) {
-	var bench = new Benchmark('abraxas-' + sz, {
+function createBench(benchmarkOptions) {
+	return {
+		'name': benchmarkOptions.name,
 		'defer': true,
 		'maxTime': 60,
 		'minRuns': 20,
 		'initCount': 3,
-		'fn': (deferred) => pushJobs(sz).then((results) => {
+		'fn': (deferred) => pushJobs(benchmarkOptions.jobsCount, benchmarkOptions.abraxasOptions).then((results) => {
 			results.forEach(result => assert.equal(result, 'ABC'))
 			deferred.resolve()
-		})
-	})
-		.on('cycle', function (event) { process.stdout.write('\r' + String(event.target)) + "            " })
-		.on('complete', function (event) {
-			var summary = sumStats(bench.stats.sample.map((x) => x / sz))
-			console.log('\n')
-			console.log("%s,%s,%s", summary.median.toPrecision(3), (summary.median - summary.q1).toPrecision(2), (summary.q3 - summary.median).toPrecision(2))
-		})
-	return bench
+		}),
+		'onCycle': function (event) { process.stdout.write('\r' + String(event.target)) + "            " },
+		'onComplete': function (event) { process.stdout.write('\n') }
+	}
 }
 
-pushJobs(150).then(() => pushJobs(150).then(() => goBench(100).run({ async: false })))
+function goBench(benchmarksOptions) {
+	var suite = new Benchmark.Suite;
+	benchmarksOptions.forEach(opt => suite.add(createBench(opt)))
+	return suite
+}
+
+var benchmarksOptions = [
+	{ name: 'abraxas jobs=100, queue=10, active=10', jobsCount: 100, abraxasOptions: { maxQueued: 10, maxJobs: 10, defaultEncoding: 'utf8' } },
+	{ name: 'abraxas jobs=100, queue=20, active=10', jobsCount: 100, abraxasOptions: { maxQueued: 20, maxJobs: 10, defaultEncoding: 'utf8' } },
+	{ name: 'abraxas jobs=100, queue=40, active=10', jobsCount: 100, abraxasOptions: { maxQueued: 40, maxJobs: 10, defaultEncoding: 'utf8' } }]
+
+var defaultAbraxasOptions = { maxQueued: 30, maxJobs: 10, defaultEncoding: 'utf8' }
+pushJobs(150, defaultAbraxasOptions)
+	.then(() => pushJobs(150, defaultAbraxasOptions)
+		.then(() => goBench(benchmarksOptions)
+			.run({ async: false })
+			.on('complete', (event) => { createPythonFile(event.currentTarget, benchmarksOptions) }) ))
