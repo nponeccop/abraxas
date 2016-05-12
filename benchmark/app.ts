900a98371550636b233a57c36aec967e8007233d
streamcode9@gmail.com <streamcode9@gmail.com>
2016-05-13 02:50:21 +0300

    Hot and dispose each client in the benchmarks

--- benchmark/app.ts
+++ benchmark/app.ts
@@ -34,37 +34,65 @@ function createPythonFile(suiteResults, benchmarksOpts) {
 	fs.writeFile('results.py', code, 'utf8', err => { if (err) throw err; console.log('It\'s saved!') })
 }
 
-function pushJobs(jobsCount, abraxasOptions) {
-	var client = ab.Client.connect(abraxasOptions)
-	client.registerWorker(tube, (job) => {
-		setTimeout(() => job.end(job.payload.toUpperCase()), 100)
-	})
-
+function pushJobs(jobsCount, abraxasClient) {
 	var jobs = []
-	for (var i = 0; i < jobsCount; i++) jobs.push(client.submitJob(tube, 'abc'))
+	for (var i = 0; i < jobsCount; i++) jobs.push(abraxasClient.submitJob(tube, 'abc'))
 	return Promise.all(jobs)
 }
 
-function createBench(benchmarkOptions) {
-	return {
-		'name': benchmarkOptions.name,
-		'defer': true,
-		'maxTime': 60,
-		'minRuns': 20,
-		'initCount': 3,
-		'fn': (deferred) => pushJobs(benchmarkOptions.jobsCount, benchmarkOptions.abraxasOptions).then((results) => {
+function getHotClient(benchmarkOptions) {
+	var client = ab.Client.connect(benchmarkOptions.abraxasOptions)
+	client.registerWorker(tube, (job) => {
+		setTimeout(() => job.end(job.payload.toUpperCase()), 100)
+	})
+		
+	return new Promise((resolve) => {
+		pushJobs(benchmarkOptions.jobsCount, client).then(() => resolve(client))        
+    })
+}
+
+function createBench(name, jobsCount, client) {
+	return new Benchmark.Benchmark(name, {
+		defer: true,
+		maxTime: 60,
+		minRuns: 20,
+		initCount: 3,
+		fn: (deferred) => pushJobs(jobsCount, client).then((results) => {
 			results.forEach(result => assert.equal(result, 'ABC'))
 			deferred.resolve()
 		}),
-		'onCycle': function (event) { process.stdout.write('\r' + String(event.target)) + "            " },
-		'onComplete': function (event) { process.stdout.write('\n') }
-	}
+		onCycle: function (event) { process.stdout.write('\r' + String(event.target)) + "            " }		
+	})
 }
 
-function goBench(benchmarksOptions) {
-	var suite = new Benchmark.Suite;
-	benchmarksOptions.forEach(opt => suite.add(createBench(opt)))
-	return suite
+function goBench(benchmarkOptions) {
+	return new Promise((resolve) => {
+		getHotClient(benchmarkOptions).then(client => {
+			var bench = createBench(benchmarkOptions.name, benchmarkOptions.jobsCount, client).run({async: false})
+			bench.on('complete', () => {
+				process.stdout.write('\n')
+				client.forgetAllWorkers()
+				client.disconnect()
+				resolve(bench)
+			})
+		})
+    })
+}
+
+function goBenchmarks(benchmarksOptions) {	
+	var results = []
+	return new Promise(resolve => {
+		goBench(benchmarksOptions[0]).then(result0 => {
+			results.push(result0)
+			goBench(benchmarksOptions[1]).then(result1 => {
+				results.push(result1)
+				goBench(benchmarksOptions[2]).then(result2 => {
+					results.push(result2)
+					resolve(results)
+				})
+			})
+		})
+	})	
 }
 
 var benchmarksOptions = [
@@ -72,9 +100,4 @@ var benchmarksOptions = [
 	{ name: 'abraxas jobs=100, queue=20, active=10', jobsCount: 100, abraxasOptions: { maxQueued: 20, maxJobs: 10, defaultEncoding: 'utf8' } },
 	{ name: 'abraxas jobs=100, queue=40, active=10', jobsCount: 100, abraxasOptions: { maxQueued: 40, maxJobs: 10, defaultEncoding: 'utf8' } }]
 
-var defaultAbraxasOptions = { maxQueued: 30, maxJobs: 10, defaultEncoding: 'utf8' }
-pushJobs(150, defaultAbraxasOptions)
-	.then(() => pushJobs(150, defaultAbraxasOptions)
-		.then(() => goBench(benchmarksOptions)
-			.run({ async: false })
-			.on('complete', (event) => { createPythonFile(event.currentTarget, benchmarksOptions) }) ))
+goBenchmarks(benchmarksOptions).then(results => createPythonFile(results, benchmarksOptions))
